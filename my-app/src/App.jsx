7628291a53fde568449f3146a96d8c156b292af7
Zahra Alvandi <zalvandi39@gmail.com>
2025-08-24 10:26:07 +0330

    conditions with props

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -8,9 +8,9 @@ function App() {
 
   return (
   <div className='container'>
-    <Product title= "Samsung A34" img="public/img1.jpg" price={120}></Product>
-    <Product title= "Samsung A51" img="public/img2.jpg" price={180}></Product>
-    <Product title= "Note 10" img="public/img3.jpg" price={295}></Product>
+    <Product title= "Samsung A34" img="public/img1.jpg" price={128} count={12}></Product>
+    <Product title= "Samsung A51" img="public/img2.jpg" price={180} count={4}></Product>
+    <Product title= "Note 10" img="public/img3.jpg" price={295} count={0}></Product>
   </div>
   )
 }
--- my-app/src/Product/product.jsx
+++ my-app/src/Product/product.jsx
@@ -1,35 +1,11 @@
 import "./product.css"
 
 function Product(props) {
-    console.log(props)
-    return (
-        // <div className="container">
-        //     <div className="card">
-        //     <h2>Product Card</h2>
-        //         <img src="img1.jpg" alt="" />
-        //         <h1>Samsung A51</h1>
-        //         <p className="price">$300</p>
-        //         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, fugit.</p>
-        //         <p><button>Add To Cart</button></p>
-        //     </div>
-        //     <div className="card">
-        //      <h2>Product Card</h2>
-        //         <img src="img2.jpg" alt="" />
-        //         <h1>Samsung A34</h1>
-        //         <p className="price">$1000</p>
-        //         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, fugit.</p>
-        //         <p><button>Add To Cart</button></p>
-        //     </div>
-        //     <div className="card">
-        //      <h2>Product Card</h2>
-        //         <img src="img3.jpg" alt="" />
-        //         <h1>Note 10</h1>
-        //         <p className="price">$1300</p>
-        //         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, fugit.</p>
-        //         <p><button>Add To Cart</button></p>
-        //     </div>
-        // </div>
-        <div>
+    console.log(props);
+
+    return(
+        props.count &&
+            <div>
             <div className="card">
              <h2>{props.title}</h2>
                  <img src={props.img} alt="" />
@@ -40,6 +16,61 @@ function Product(props) {
              </div>
         </div>
     )
+    
+
+    // if(props.count > 0) {
+        // return(
+        //     <div>
+        //     <div className="card">
+        //      <h2>{props.title}</h2>
+        //          <img src={props.img} alt="" />
+        //          <h1>Samsung A51</h1>
+        //          <p className="price">{`$${props.price}`}</p>
+        //          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, fugit.</p>
+        //          <p><button>Add To Cart</button></p>
+        //      </div>
+        // </div>
+        // )
+    // }
+    // return null
+    // return (
+    //     // <div className="container">
+    //     //     <div className="card">
+    //     //     <h2>Product Card</h2>
+    //     //         <img src="img1.jpg" alt="" />
+    //     //         <h1>Samsung A51</h1>
+    //     //         <p className="price">$300</p>
+    //     //         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, fugit.</p>
+    //     //         <p><button>Add To Cart</button></p>
+    //     //     </div>
+    //     //     <div className="card">
+    //     //      <h2>Product Card</h2>
+    //     //         <img src="img2.jpg" alt="" />
+    //     //         <h1>Samsung A34</h1>
+    //     //         <p className="price">$1000</p>
+    //     //         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, fugit.</p>
+    //     //         <p><button>Add To Cart</button></p>
+    //     //     </div>
+    //     //     <div className="card">
+    //     //      <h2>Product Card</h2>
+    //     //         <img src="img3.jpg" alt="" />
+    //     //         <h1>Note 10</h1>
+    //     //         <p className="price">$1300</p>
+    //     //         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, fugit.</p>
+    //     //         <p><button>Add To Cart</button></p>
+    //     //     </div>
+    //     // </div>
+    //     // <div>
+    //     //     <div className="card">
+    //     //      <h2>{props.title}</h2>
+    //     //          <img src={props.img} alt="" />
+    //     //          <h1>Samsung A51</h1>
+    //     //          <p className="price">{`$${props.price}`}</p>
+    //     //          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, fugit.</p>
+    //     //          <p><button>Add To Cart</button></p>
+    //     //      </div>
+    //     // </div>
+    // )
 }
 
 export default Product
